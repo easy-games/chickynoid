4ef59ba80e09632b66e5c2fbccf897fe0093668a
grilme99 <brooke@gril.me>
2022-05-05 18:46:28 +0100

    Use exported modules

--- example/client/chickynoid.client.lua
+++ example/client/chickynoid.client.lua
@@ -1,6 +1,6 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local Packages = ReplicatedStorage.Packages
-local Chickynoid = require(Packages.Chickynoid.Client)
+local Chickynoid = require(Packages.Chickynoid).ChickynoidClient
 
 Chickynoid:Setup()
--- example/server/chickynoid.server.lua
+++ example/server/chickynoid.server.lua
@@ -1,7 +1,7 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local Packages = ReplicatedStorage.Packages
-local Chickynoid = require(Packages.Chickynoid.Server)
+local Chickynoid = require(Packages.Chickynoid).ChickynoidServer
 
 Chickynoid:RecreateCollisions(workspace:FindFirstChild("GameArea"))
 Chickynoid:Setup()

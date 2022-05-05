47fce5c011b5b49b626bc9b8dbfbc150783b7189
MrChickenRocket <rgoslin80@gmail.com>
2022-05-05 15:13:55 +1000

    Refined on the fly terrain collision generation some. Still not perfect.

--- DataModel/ReplicatedFirst/Packages/Chickynoid/Simulation/TerrainCollision.lua
+++ DataModel/ReplicatedFirst/Packages/Chickynoid/Simulation/TerrainCollision.lua
@@ -42,68 +42,123 @@ function module:FetchCell(x,y,z)
 	
 	 
 	local cell = self:CreateAndFetchCell(x,y,z)
-	
-	self.testPart.Parent = game.Workspace
-	
+	 
 	local max = self.div-1
+	
+	local corner = Vector3.new(x,y,z)*self.gridSize
+	local region = Region3.new(corner, corner + Vector3.new(self.gridSize+4,self.gridSize+4,self.gridSize+4))
+
+	local materials,occs = game.Workspace.Terrain:ReadVoxels(region,4)
+
+	local step = 4 / self.boxSize
+	
+	local list = {}
+	
 	for xx=0,max do
 		for yy=0,max do
 			for zz=0,max do
 				
+				local fraction = Vector3.new ( x+(xx / self.div), y+(yy / self.div), z+(zz / self.div) )
+				local fill = self:GetOccupancyBilinear(occs, (xx / step) ,( yy /step) , (zz / step) )
 				
-				local center = (Vector3.new(x+(xx / self.div), y+(yy / self.div), z+(zz / self.div))*self.gridSize) + (Vector3.new(self.boxSize,self.boxSize,self.boxSize)*0.5)
-				
-				self.testPart.Position = center
-				
-				local parts = self.testPart:GetTouchingParts()
-				if (#parts == 0) then
-					continue
-				end
-				
-				for key,value in pairs(parts) do
-					if value == game.Workspace.Terrain then
+				if (fill > 0.2) then
+					local center = (fraction * self.gridSize) + (Vector3.new(self.boxSize,self.boxSize,self.boxSize)*0.5) 
+
 					
-						--we're good
-						local list = {}
-						for a,corner in pairs(self.expandedCorners) do
-							
-							table.insert(list, center + corner)
-						end	
+					for a,corner in pairs(self.expandedCorners) do
 
-						local hull, planeNum = MinkowskiSumInstance:GetPlanesForPoints(list, self.planeNum)
-						self.planeNum = planeNum
-						if (hull and planeNum) then
+						table.insert(list, center + corner)
+					end	
 
-							--CollisionModule.planeNum = planeNum 
-							table.insert(cell,  { hull = hull } ) 
-						end
-						
-					end
+			
+					
+					--[[if (game["Run Service"]:IsClient()) then
+						self:SpawnDebugGridBox(fraction.x, fraction.y, fraction.z , Color3.fromHSV(math.random(),1,1), self.boxSize)
+					end]]--
 				end
-				--[[
-				local list = {}
 				
-				
-				
-				local hull, planeNum = MinkowskiSumInstance:GetPlanesForPointsExpanded(list, self.expansionSize, self.planeNum)
-				self.planeNum = planeNum
-				if (hull and planeNum) then
-										
-					--CollisionModule.planeNum = planeNum 
-					cell[hull] = hull
-				end
-				]]--
-				
-				--self:SpawnDebugGridBox(x+(xx / self.div), y+(yy / self.div), z+(zz / self.div), Color3.fromHSV(math.random(),1,1), 4)
+		
 			end
 		end
 	end
-	self.testPart.Parent = nil
+	
+	if (#list > 0) then
+		local hull, planeNum = MinkowskiSumInstance:GetPlanesForPoints(list, self.planeNum)
+		self.planeNum = planeNum
+		if (hull and planeNum) then
+
+			--CollisionModule.planeNum = planeNum 
+			table.insert(cell,  { hull = hull } ) 
+		end
+	end
 	
 	--self:SpawnDebugGridBox(x, y, z, Color3.fromHSV(math.random(),1,1), self.gridSize)
 	
 	return cell
 	
+end
+
+local function lerp(a,b,frac)
+	return (a*(1-frac)) + (b*(frac))
+end
+
+function module:GetOccupancyBilinear(occ, localx,localy,localz)
+	
+	
+	localx+=1
+	localy+=1
+	localz+=1
+	
+	local x = math.floor(localx)
+	local y = math.floor(localy)
+	local z = math.floor(localz)
+	
+	
+ 
+	
+	--    botface
+	--
+	--     c -----fx---cd--- d		
+	--                  |
+	--                  |
+	--                  |
+	--                  |        
+	--                  fy        ^
+	--                  |         |
+	--     a -----fx---ab---- b   |
+	--                            |
+	--     (xaxis)---->           (zaxis)
+
+
+	local fx = localx - x
+	local fy = localy - y
+	local fz = localz - z
+
+	--Bot face samples
+	local a_bot = occ[x+0][y+0][z+0]
+	local b_bot = occ[x+1][y+0][z+0]
+	local c_bot = occ[x+0][y+0][z+1]
+	local d_bot = occ[x+1][y+0][z+1]
+
+	--Top face samples
+	local a_top = occ[x+0][y+1][z+0]
+	local b_top = occ[x+1][y+1][z+0]
+	local c_top = occ[x+0][y+1][z+1]
+	local d_top = occ[x+1][y+1][z+1]
+
+	--Bot face lerped
+	local ab_bot = lerp(a_bot, b_bot, fx)
+	local cd_bot = lerp(c_bot, d_bot, fx)
+	local botFace = lerp(ab_bot, cd_bot, fz)
+
+	--Top face lerped
+	local ab_top = lerp(a_top, b_top, fx)
+	local cd_top = lerp(c_top, d_top, fx)
+	local topFace = lerp(ab_top, cd_top, fz)
+
+	--Between bot and top face
+	return lerp(botFace, topFace, fy)  
+
 end
 
 function module:SpawnDebugGridBox(x,y,z, color, grid)
@@ -169,29 +224,15 @@ function module:Setup(gridSize, expansionSize)
 	
 	self.grid = {}
 	self.expansionSize = expansionSize
-	
-	self.boxSize = 2 --resolution of the box parts	
-	local part = Instance.new("Part")
-	part.CanCollide = false
  
-	part.Size = Vector3.new(self.boxSize,self.boxSize,self.boxSize)
-	part.Anchored = true
-	part.Touched:Connect(function()
-	end)
-	 
-	self.testPart = part
-	
 	self.gridSize = gridSize
+	self.boxSize = 2
+	self.div = self.gridSize / self.boxSize
 	
-	self.div = math.floor(gridSize/self.boxSize)
-	local a,rem = math.modf(gridSize/self.boxSize)
-	if (rem > 0) then
-		error("Collision Module gridsize should match the terrain and be divisible by boxSize")
-	end
-		
+
 	self.expandedCorners = {}
 	for key,corner in pairs(corners) do
-		table.insert(self.expandedCorners,  (corner * self.boxSize * self.expansionSize))
+		table.insert(self.expandedCorners,  (corner * self.boxSize) + (corner * self.expansionSize ))
 	end
 end
 

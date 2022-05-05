882ff9622e7b0a2d8481cccd557e03e0f04e7b5e
grilme99 <brooke@gril.me>
2022-05-05 20:23:51 +0100

    Fix exports

--- src/init.lua
+++ src/init.lua
@@ -1,9 +1,14 @@
-local Client = require(script.Client)
-local Server = require(script.Server)
+local RunService = game:GetService("RunService")
 
 local Chickynoid = {}
 
-Chickynoid.ChickynoidClient = Client
-Chickynoid.ChickynoidServer = Server
+local IsServer = RunService:IsServer()
+if IsServer then
+    local Server = require(script.Server)
+    Chickynoid.ChickynoidServer = Server
+else
+    local Client = require(script.Client)
+    Chickynoid.ChickynoidClient = Client
+end
 
 return Chickynoid

19aed7b86ef549b5cd4d0e3de2487721134e954a
Luke Chatton <chattonluke@gmail.com>
2022-05-17 17:07:16 -0700

    weapons client

---
+++ types/Client/WeaponsClient.d.ts
@@ -0,0 +1,9 @@
+import Signal from "@rbxts/signal";
+import { ChickynoidClient } from ".";
+import { WeaponModule } from "../Shared/WeaponModule";
+
+export namespace WeaponsClient {
+	export let OnBulletImpact: Signal<(client: typeof ChickynoidClient, event: unknown) => void>;
+
+	export function GetWeaponModuleByWeaponId(self: typeof WeaponsClient, weaponId: number): WeaponModule;
+}

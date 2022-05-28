a2c3c7b35aff23b5ad3fa3e635a9219fe59658a1
Luke Chatton <chattonluke@gmail.com>
2022-05-27 17:54:50 -0700

    oops

--- example/customclient/ClientMods/NetgraphHotkeys.lua
+++ example/customclient/ClientMods/NetgraphHotkeys.lua
@@ -5,8 +5,8 @@ local UserInputService = game:GetService("UserInputService")
 
 function module:Setup(_client)
     self.client = _client
-    _client.showFpsGraph = false
-    _client.showNetGraph = false
+    _client.showFpsGraph = true
+    _client.showNetGraph = true
 end
 
 function module:Step(_client, _deltaTime)

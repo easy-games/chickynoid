ba1e1c52378d12735b5cb7bfb0b83ace24c0573e
Luke Chatton <chattonluke@gmail.com>
2022-05-30 15:50:36 -0700

    add OnLoadProgressChanged signal

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,6 +2,7 @@
 export * from "./Client";
 /** @server  */
 export * from "./Server";
+export * from "./Shared/CollisionModule";
 export * from "./Shared/MathUtils";
 export * from "./Simulation/simulation-constants";
 export * from "./Simulation/simulation-state";

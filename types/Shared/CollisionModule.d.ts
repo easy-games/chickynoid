879237dc77fd35d98ec9c7d4bcad7f02c572ae64
Luke Chatton <chattonluke@gmail.com>
2022-05-30 15:56:34 -0700

    didn't mean to include this

--- types/Shared/CollisionModule.d.ts
+++ types/Shared/CollisionModule.d.ts
@@ -1,22 +1,6 @@
 import Signal from "@rbxts/signal";
-import { MathUtils } from "./MathUtils";
 
 export namespace CollisionModule {
 	export let loadProgress: number;
 	export const OnLoadProgressChanged: Signal<(progress: number) => void>;
-
-	export function GroundAccelerate(
-		this: typeof MathUtils,
-		wishDir: Vector3,
-		wishSpeed: number,
-		accel: number,
-		velocity: Vector3,
-		dt: number,
-	): Vector3;
-
-	export function VelocityFriction(this: typeof MathUtils, vel: Vector3, fric: number, dt: number): Vector3;
-
-	export function PlayerVecToAngle(this: typeof MathUtils, vec: Vector3): number;
-
-	export function LerpAngle(this: typeof MathUtils, a0: number, a1: number, frac: number): number;
 }

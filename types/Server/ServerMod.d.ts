fa339d3aa9999dccc68e00d2a2a4d86da20dede2
Luke Chatton <chattonluke@gmail.com>
2022-06-09 10:44:23 -0700

    add CanPlayerSee

--- types/Server/ServerMod.d.ts
+++ types/Server/ServerMod.d.ts
@@ -1,7 +1,10 @@
 import { ChickynoidServer } from ".";
+import PlayerRecord from "./PlayerRecord";
 
 export interface ServerMod {
 	Step(server: typeof ChickynoidServer, deltaTime: number): void;
 
 	Setup(server: typeof ChickynoidServer): void;
+
+	CanPlayerSee?(sourcePlayer: PlayerRecord, otherPlayer: PlayerRecord): boolean;
 }

426c0daabb068efac1b43f09dc1a455c26fe0c03
Luke Chatton <chattonluke@gmail.com>
2022-07-04 10:21:13 -0700

    add ClearCache()

--- types/Shared/CollisionModule.d.ts
+++ types/Shared/CollisionModule.d.ts
@@ -5,4 +5,5 @@ export namespace CollisionModule {
 	export const OnLoadProgressChanged: Signal<(progress: number) => void>;
 
 	export function ProcessCollisionOnInstance(this: typeof CollisionModule, instance: Instance): void;
+	export function ClearCache(this: typeof CollisionModule): void;
 }
